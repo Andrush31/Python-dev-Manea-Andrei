4d5c58f1c9c1695839d58b6f1b140352d2f48a82
Andrush31 <151433066+Andrush31@users.noreply.github.com>
2024-09-23 14:34:28 +0300

    Top Filme

--- Tema02/Top Filme.py
+++ Tema02/Top Filme.py
@@ -1,12 +1,3 @@
-# Avand o lista de utilizatori si filme vizionate, listati
-# Cel mai vizionat film - Fight Club in cazul de mai sus
-# Utilizatorul cu cele mai multe filme vizionate - Cristian in cazul de mai sus
-# Extra
-# Top filme dupa vizionari: Fight Club, Bond, Dracula, Shrek, The nun ...
-# Top utilizatori cu cele mai multe filme vizionate - Cristian, George, Stefan
-from itertools import count
-from operator import index
-
 lista = [ { 'nume': 'George',
    'filme': ['Shrek', 'Bond', 'Fight Club']},
 
@@ -16,17 +7,31 @@ lista = [ { 'nume': 'George',
 {'nume' : 'Stefan',
  'filme': ['Fight Club', 'Slumdog Millionaire'] } ]
 lista_filme = []
-suma = 0
-vect = 0
-xee = 0
-index_lista = {0}
-for i in lista:
-    lista_filme= list(set(lista_filme).union(set( i['filme'])))
+for i in lista: #lista de referinta
+    lista_filme = list(set(lista_filme).union(set(i['filme'])))
+zeros_list = [0] * len(lista_filme)
+vect_util = [0] * len(lista)
+for j,i in enumerate(lista):  #pentru a afla cel mai vizionat film
+    # lista_filme= list(set(lista_filme).union(set( i['filme'])))
     lista_2 = list(i['filme'])
-    for l, k in enumerate(lista_filme):
-        if l == index(list(lista_filme)):
-            continue
-        index_lista = list(index_lista.add(0))
+    # zeros_list = [0] * len(lista_filme)
+    # print(lista_filme)
+    for l, k in enumerate(list(i['filme'])):
+    # print(l, k)
+        for index_l_f, value_l_f in enumerate(lista_filme):
+            # print(index_l_f)
+            if k == value_l_f:
+                zeros_list[index_l_f] = zeros_list[index_l_f]  + 1
+                # print(lista_filme)
 
-print(vect)
-print(lista_filme)
+    for l in lista_2:
+        vect_util[j] += 1
+
+# print(vect_util.index(max(vect_util)))
+print(f"utiliztorul cu cele mai multe filme vizionate este {lista[vect_util.index(max(vect_util))]['nume']}")
+maxim = zeros_list.index(max(zeros_list))
+print(f"Cel mai vizionat film este {lista_filme[maxim]}, cu {max(zeros_list)} vizionari")
+
+
+# print(zeros_list)
+# print(lista_filme)
